6f33d44390865e9e49cc6761ffc524bf7c99a5db
natca <ncatundafr@gmail.com>
2026-01-08 13:40:55 -0300

    second commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,19 @@
-fun main(){
+fun main() {
     println("Login")
-    println("Digite senha")
-    val  senha: String = readln() //entra com senha
-    val regex = Regex("[!@#%]"+ "[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]") //filtra a string por esses caracteres
-    if (regex.containsMatchIn(senha)) {
-        println("Senha contém caractere especial")
-    } // associa a variável imutavel á o regex perguntando se esses caracteres estão no input digitado.
-        when  (senha==""){
-                    else-> {
-                        println("Digite senha")
-                        println("Senha incorreta, digite ao menos um maiuscula e um caráctere especial")
-                    } // caso não... senha incorreta.
+    println("Digite senha\n")
+    val senha = readln()
+    val regexEspecial = Regex("[!@#%]")// regex para caracteres especiais
+    val regexUppercase = Regex("[A-Z]") // regex para Uppercase Alfabetico
+    if (regexUppercase.containsMatchIn(senha) && regexEspecial.containsMatchIn(senha)) {
+        println("Bem vindo")
+    }// filtra por input
+        else {
+            println("Digite senha novamente")
+            println("Senha incorreta, digite ao menos um maiuscula e um caráctere especial")
+        } // caso não... senha incorreta.
 
-
-        }
 
     }
+
+
 
